636faea395086fc57dec1032a4dae2f9f62cc768
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-06-01 19:43:57 -0300

    testes de rotas de cidade e configuração do knex

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -6,23 +6,15 @@ import { validation } from '../../shared/middleware';
 interface ICity { // Tipo do body da request
   name: string;
 }
-interface IFilter {
-  filter?: string;
-}
 
 export const createValidation = validation((getSchema) => ({ // Middleware que irá validar body, params e query.
   body: getSchema<ICity>(yup.object().shape({ //Shape dos campos desejados
     name: yup.string().required().min(3),
-  })),
-  query: getSchema<IFilter>(yup.object().shape({ //Shape dos campos desejados
-    filter: yup.string().optional().min(3)
   }))
 }));
 
 
 // 2 objetos vazios pq o ReqBody tem que ficar na 3° posição, só passar o mouse em cima de Request para ver
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
-  // let validatedData: ICity | undefined = undefined;
-  console.log(req.body);
-  return res.send('Create')
+  return res.status(201).json(1);
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -7,13 +7,11 @@ router.get('/', (req, res) => {
   return res.send('Rota de teste! Utilize os outros endpoints.');
 });
 
-router.post(
-  '/cities',
-  // CitiesController.createQueryValidation,
-  // CitiesController.createValidation,
-  CitiesController.createValidation,
-  CitiesController.create
-);
+router.post('/cities',CitiesController.createValidation,CitiesController.create);
+router.get('/cities',CitiesController.getAllValidation,CitiesController.getAll);
+router.get('/cities/:id',CitiesController.getByIdValidation,CitiesController.getById);
+router.put('/cities/:id',CitiesController.updateByIdValidation,CitiesController.updateById);
+router.delete('/cities/:id',CitiesController.deleteByIdValidation,CitiesController.deleteById);
 
 
 export { router };
