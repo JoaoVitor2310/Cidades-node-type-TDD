d40f5107fdd42f7af8e0d151c15a6de37137f00f
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-06-03 18:40:34 -0300

    configuração do knex

---
+++ src/server/database/ETableNames.ts
@@ -0,0 +1,4 @@
+// Enum, terá o nome de todas as tabelas para simplificar o uso delas.
+export enum ETableNames {
+  city = 'city'
+}
---
+++ src/server/database/migrations/0000_create_city.ts
@@ -0,0 +1,23 @@
+import { Knex } from 'knex';
+import { ETableNames } from '../ETablenames';
+
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable(
+    ETableNames.city, table => {
+      table.bigIncrements('id').primary().index(); // Coluna id será de incremento, chave primária e index de pesquisa.
+      table.string('name', 150).index().notNullable(); // Coluna nome será string, máx de 150 caracteres, index de pesquisa e não nulo.
+      table.comment('Table used to record cities names'); // Comentário da tabela.
+    }).then(() => {
+    console.log(`# Create table ${ETableNames.city}`);
+  });
+
+}
+
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETableNames.city).then(() => { // Exclui a tabela de dados quando for solicitada.
+    console.log(`# Create table ${ETableNames.city}`);
+  });
+}
+
